1b304ec3b3f6ed7e88409f5b26ec584122a8f154
w0x7ce <tian.r.king@gmail.com>
2022-12-13 12:56:29 +0800

    Update: oled u8g2

---
+++ README.md
@@ -0,0 +1,1 @@
+Seeediuno_Zero 
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,36 @@
 #include<Arduino.h>
+#include <U8g2lib.h>
+//https://registry.platformio.org/libraries/olikraus/U8g2/installation
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+U8G2_SSD1306_128X64_ALT0_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); 
 
 void setup() {
-  // put your setup code here, to run once:
-    Serial.begin(115200);
-    pinMode(D5, OUTPUT); //Set pin 13 as an 'output' pin as we will make it output a voltage.
-    // digitalWrite(13, HIGH); //This turns on pin 13/supplies it with 3.3 Volts.
 
-    // pinMode(11, OUTPUT);
-    // analogWrite(11, 127); 
-    // pinMode(11, INPUT);
+    Serial.begin(115200);
+    pinMode(D5, OUTPUT); 
+
+    u8g2.begin();
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+
     Serial.println("Hello world!");
-    digitalWrite(D5, HIGH); //Step 1: The LED Turns on.
+    digitalWrite(D5, HIGH);
     delay(1000);
     digitalWrite(D5, LOW);
     delay(1000);
-    // int tstate = digitalRead(11);
+    u8g2.clearBuffer();                   // clear the internal memory
+    u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
+    u8g2.drawStr(0,10,"Hello World!");    // write something to the internal memory
+    u8g2.sendBuffer();                    // transfer internal memory to the display
+    delay(1000);  
+    
 }
 
